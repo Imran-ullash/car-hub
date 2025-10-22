085a257ebc5bc17bbb07a237df9ba0ec26670433
Imran-ullash <bdcallingullash@gmail.com>
2025-10-23 05:25:41 +0600

    up to date

---
+++ src/app/(auth-page)/register/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            <h1>this is register page</h1>
+        </div>
+    );
+};
+
+export default page;
---
+++ src/app/(dashboard)/layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const dasdboadLayout = ({children}) => {
+    return (
+        <div className='bg-red-200'>
+
+            {children}
+        </div>
+    );
+};
+
+export default dasdboadLayout;
